d2e07ca0f4bfb5737bd8d191ed0926fbeaf1f9b0
Elias <eliashnf@gmail.com>
2023-01-18 18:30:18 +0100

    first commit

--- src/Views/Public/Forgot.tsx
+++ src/Views/Public/Forgot.tsx
@@ -2,23 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Forgot() {
-  return(
+  return (
     <div>
-    <form>
-      <h1>Forgot</h1>
-      <label>
-        <p>Username</p>
-        <input type="text" />
-      </label>
-      <label>
-        <p>Password</p>
-        <input type="password" />
-      </label>
-      <div>
-        <button type="submit">Submit</button>
-      </div>
-    </form>
-    <Link to="/login">Login </Link>
+      <form>
+        <h1>Forgot</h1>
+        <label>
+          <p>Username</p>
+          <input type="text" />
+        </label>
+        <label>
+          <p>Password</p>
+          <input type="password" />
+        </label>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+      <Link to="/login">Login </Link>
     </div>
   )
 }
